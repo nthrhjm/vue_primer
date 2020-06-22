a41c39236fa2e345a793fa4f44d9f90b77085e07
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-06-23 00:23:05 +0900

    v-bindで属性値を変える

---
+++ chapter_3/index.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: "#app",
+  data: {
+    book: {
+      title: "Pythonで機械学習入門 深層学習から敵対的生成ネットワークまで",
+      author: "大関 真之",
+      url: "https://www.ohmsha.co.jp/book/9784274222863/",
+    },
+  },
+});
